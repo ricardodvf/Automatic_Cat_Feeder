3aca7a4972095f69e400b4bc5c1c8ff4f121d26c
Ricardo Del Valle <ricardodvf@outlook.com>
2022-01-24 09:56:49 -0600

    1/24/2022

--- src/main.cpp
+++ src/main.cpp
@@ -6,48 +6,47 @@
 #include <ESP32Servo.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include "SPIFFS.h"
 
 //Functions declarations
 
-
 //#include <WiFi.h> //For the server function
 // Set web server port number to 80
-AsyncWebServer  server(80);
+AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
 
-// Variable to store the HTTP request
-String header;
 // Current time
 unsigned long currentTime = millis();
 // Previous time
-unsigned long previousTime = 0; 
+unsigned long previousTime = 0;
 // Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
 
-Servo myservo;  // create servo object to control a servo
-// 16 servo objects can be created on the ESP32
- 
-int pos = 0;    // variable to store the servo position
-// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33 
+Servo myservo; // create servo object to control a servo
+
+int pos = 0; // variable to store the servo position
+
 int servoPin = 18;
 
 bool catFeederStatus = 0; //Variable to indicate if cat feeder is on or off
 
-const char* ssid = "*****";
-const char* password = "******";
-
-
+const char *ssid = "*****";
+const char *password = "******";
 
 //***************************************************************************************************
-void notifyClients() {
+void notifyClients()
+{
   ws.textAll(String("Something"));
 }
 //***************************************************************************************************
-void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
-  AwsFrameInfo *info = (AwsFrameInfo*)arg;
-  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
+{
+  AwsFrameInfo *info = (AwsFrameInfo *)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT)
+  {
     data[len] = 0;
-    if (strcmp((char*)data, "toggle") == 0) {
+    if (strcmp((char *)data, "toggle") == 0)
+    {
       ledState = !ledState;
       notifyClients();
     }
@@ -56,36 +55,43 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
 //***************************************************************************************************
 
 void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
-             void *arg, uint8_t *data, size_t len) {
-  switch (type) {
-    case WS_EVT_CONNECT:
-      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
-      break;
-    case WS_EVT_DISCONNECT:
-      Serial.printf("WebSocket client #%u disconnected\n", client->id());
-      break;
-    case WS_EVT_DATA:
-      handleWebSocketMessage(arg, data, len);
-      break;
-    case WS_EVT_PONG:
-    case WS_EVT_ERROR:
-      break;
+             void *arg, uint8_t *data, size_t len)
+{
+  switch (type)
+  {
+  case WS_EVT_CONNECT:
+    Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+    break;
+  case WS_EVT_DISCONNECT:
+    Serial.printf("WebSocket client #%u disconnected\n", client->id());
+    break;
+  case WS_EVT_DATA:
+    handleWebSocketMessage(arg, data, len);
+    break;
+  case WS_EVT_PONG:
+  case WS_EVT_ERROR:
+    break;
   }
 }
 //***************************************************************************************************
-void initWebSocket() {
+void initWebSocket()
+{
   ws.onEvent(onEvent);
   server.addHandler(&ws);
 }
 
 //***************************************************************************************************
-String processor(const String& var){
+String processor(const String &var)
+{
   Serial.println(var);
-  if(var == "STATE"){
-    if(digitalRead(3)){
+  if (var == "STATE")
+  {
+    if (digitalRead(3))
+    {
       catFeederStatus = 1;
     }
-    else{
+    else
+    {
       catFeederStatus = 2;
     }
     Serial.print(catFeederStatus);
@@ -94,74 +100,89 @@ String processor(const String& var){
   return String();
 }
 //***************************************************************************************************
-void setup() {
-  Serial.begin(115200); 
+void setup()
+{
+  Serial.begin(115200);
   Serial.println("Booting");
+  // Initialize the internal SPISS memory on the ESP32
+  if (!SPIFFS.begin(true))
+  {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+
+  //Reads the WIFI Password from the SPISS
+  // Establish WiFi connections.
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
     Serial.println("Connection Failed! Rebooting...");
     delay(5000);
     ESP.restart();
   }
 
-  if(!MDNS.begin("catFeeder")) {
-     Serial.println("Error starting mDNS");
-     return;
-  }
+  
 
+  //Arduino over the are sync code
   ArduinoOTA
-    .onStart([]() {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH)
-        type = "sketch";
-      else // U_SPIFFS
-        type = "filesystem";
+      .onStart([]()
+               {
+                 String type;
+                 if (ArduinoOTA.getCommand() == U_FLASH)
+                   type = "sketch";
+                 else // U_SPIFFS
+                   type = "filesystem";
 
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type);
-    })
-    .onEnd([]() {
-      Serial.println("\nEnd");
-    })
-    .onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    })
-    .onError([](ota_error_t error) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    });
+                 // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+                 Serial.println("Start updating " + type);
+               })
+      .onEnd([]()
+             { Serial.println("\nEnd"); })
+      .onProgress([](unsigned int progress, unsigned int total)
+                  { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
+      .onError([](ota_error_t error)
+               {
+                 Serial.printf("Error[%u]: ", error);
+                 if (error == OTA_AUTH_ERROR)
+                   Serial.println("Auth Failed");
+                 else if (error == OTA_BEGIN_ERROR)
+                   Serial.println("Begin Failed");
+                 else if (error == OTA_CONNECT_ERROR)
+                   Serial.println("Connect Failed");
+                 else if (error == OTA_RECEIVE_ERROR)
+                   Serial.println("Receive Failed");
+                 else if (error == OTA_END_ERROR)
+                   Serial.println("End Failed");
+               });
 
   ArduinoOTA.begin();
 
-  Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  // My Code ************************
-  // Allow allocation of all timers
+  // Initialize the servo and associated timers. The servo is sent to default position.
   ESP32PWM::allocateTimer(0);
   ESP32PWM::allocateTimer(1);
   ESP32PWM::allocateTimer(2);
   ESP32PWM::allocateTimer(3);
-  myservo.setPeriodHertz(50);    // standard 50 hz servo
-  myservo.attach(servoPin); // attaches the servo on pin 18 to the servo object
+  myservo.setPeriodHertz(50); // standard 50 hz servo
+  myservo.attach(servoPin);   // attaches the servo on pin 18 to the servo object
   myservo.write(10);
-  //WebServer
+
+  //Start and configue the WebServer
   initWebSocket();
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
-  });
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send_P(200, "text/html", index_html, processor); });
   server.begin();
+
+  //Send our messages to the console stating we are ready to rock and roll!
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
   Serial.println("HTTP server started");
 }
 
 //***************************************************************************************************
-void loop() {
+void loop()
+{
   ArduinoOTA.handle();
 }
 
